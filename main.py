c83dec9b08595eea1bbabd0f72221fd19e047bff
Evil Developer <adrulub@yandex.ru>
2024-03-19 15:39:35 +0300

    solved third problem

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask import render_template
+from data import db_session
+from data.users import User
+from data.jobs import Jobs
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+def main():
+    app.run()
+
+
+if __name__ == '__main__':
+    main()

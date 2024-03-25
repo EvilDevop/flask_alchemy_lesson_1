130a584ddae84688e9fbdd6a2da0ee9ddd15e30b
Evil Developer <adrulub@yandex.ru>
2024-03-25 16:39:20 +0300

    2

--- main.py
+++ main.py
@@ -10,13 +10,14 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 def main():
     db_session.global_init("db/blogs.db")
-    db_sess = db_session.create_session()
-    user = db_sess.query(User).filter(User.id == 1).first()
-    news = News(title="Личная запись", content="Эта запись личная",
-                is_private=True)
-    user.news.append(news)
-    db_sess.commit()
     app.run()
+
+
+@app.route("/")
+def index():
+    db_sess = db_session.create_session()
+    news = db_sess.query(News).filter(News.is_private is not True)
+    return render_template("index.html", news=news)
 
 
 if __name__ == '__main__':

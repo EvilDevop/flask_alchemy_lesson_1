6d4930ae4632e134cebabffd665f0ea4d43c4e42
Evil Developer <adrulub@yandex.ru>
2024-03-25 16:03:40 +0300

    1

--- data/users.py
+++ data/users.py
@@ -1,7 +1,7 @@
 import datetime
 import sqlalchemy
-from .db_session import SqlAlchemyBase
 from sqlalchemy import orm
+from .db_session import SqlAlchemyBase
 
 
 class User(SqlAlchemyBase):
@@ -9,18 +9,15 @@ class User(SqlAlchemyBase):
 
     id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True, autoincrement=True)
-    surname = sqlalchemy.Column(sqlalchemy.String)
-    name = sqlalchemy.Column(sqlalchemy.String)
-    age = sqlalchemy.Column(sqlalchemy.Integer)
-    position = sqlalchemy.Column(sqlalchemy.String)
-    speciality = sqlalchemy.Column(sqlalchemy.String)
-    address = sqlalchemy.Column(sqlalchemy.String)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    about = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     email = sqlalchemy.Column(sqlalchemy.String,
                               index=True, unique=True, nullable=True)
     hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    modified_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
+    created_date = sqlalchemy.Column(sqlalchemy.DateTime,
+                                     default=datetime.datetime.now)
 
-    jobs = orm.relationship("Jobs", back_populates='user')
+    news = orm.relationship("News", back_populates='user')
 
     def __repr__(self):
-        return f'{self.id} {self.name} {self.email}'
+        return f'<Colonist> {self.id} {self.name} {self.email}'
--- main.py
+++ main.py
@@ -2,13 +2,20 @@ from flask import Flask
 from flask import render_template
 from data import db_session
 from data.users import User
-from data.jobs import Jobs
+from data.news import News
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 def main():
+    db_session.global_init("db/blogs.db")
+    db_sess = db_session.create_session()
+    user = db_sess.query(User).filter(User.id == 1).first()
+    news = News(title="Личная запись", content="Эта запись личная",
+                is_private=True)
+    user.news.append(news)
+    db_sess.commit()
     app.run()
 
 
